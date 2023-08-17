93f6fd5ea868357ae0b215d4bcb182032718fa0f
jhudle <46239234+jhudle@users.noreply.github.com>
2023-08-17 18:22:28 +0800

    Changed the style of the filter button and added responsiveness to the pages

--- src/Home.js
+++ src/Home.js
@@ -35,14 +35,14 @@ const Home = () => {
         <div className="home-container">
             <div className="filter-container">
                 <div className="checkbox-container">
-                    <input type="radio" name="location" id="all" value="All" onClick={() => handleFilter('All') }/>
-                    <label className='location-label' htmlFor="all">All</label>
+                    <input className="radio-btn" type="radio" name="location" id="all" value="All" onClick={() => handleFilter('All') }/>
+                    <label className='location-label button' htmlFor="all">All</label>
                 </div>
                 {locations &&
                     locations.map(location => (
                         <div className="checkbox-container" key={location.term_id}>
-                            <input type="radio" name="location" onClick={() => handleFilter(location.name) } id={location.name} value={location.name} />
-                            <label className='location-label' htmlFor={location.name}>{location.name}</label>
+                            <input className="radio-btn" type="radio" name="location" onClick={() => handleFilter(location.name) } id={location.name} value={location.name} />
+                            <label className='location-label button' htmlFor={location.name}>{location.name}</label>
                         </div>
                     ))
                 }
